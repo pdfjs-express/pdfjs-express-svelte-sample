d58de248ce1a898d814cec5944c4d0dd2c0fca7b
phenethylamine <dustin.riley@me.com>
2020-04-17 16:47:24 -0700

    pdfjs express svelte sample

---
+++ src/WebViewer.svelte
@@ -0,0 +1,32 @@
+<script>
+
+  import { onMount, createEventDispatcher } from 'svelte';
+  import WebViewer from '@pdftron/pdfjs-express';
+
+  let instance = null;
+  const dispatch = createEventDispatcher();
+
+	onMount(async () => {
+		const ele = document.getElementById('viewer');
+	WebViewer({
+      path: '/lib'
+		}, ele).then(instance => {
+			dispatch('ready', {
+				instance
+			})
+		})
+	});
+
+</script>
+
+<style>
+	#viewer {
+		width: 100%;
+		height: 100%;
+	}
+</style>
+
+
+<div id='viewer'>
+
+</div>
